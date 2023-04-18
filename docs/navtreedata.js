d2c4e078b3231356930fccbc1ec3f30489096753
Docs Robot <robot@example.com>
2023-04-18 08:07:15 +0000

    Committing documentation changes

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -25,9 +25,7 @@
 var NAVTREE =
 [
   [ "Soldered LCD Library", "index.html", [
-    [ "LCD driver board Arduino library", "index.html", [
-      [ "Have fun!", "index.html#autotoc_md8", null ]
-    ] ],
+    [ "LCD driver board Arduino library", "index.html", "index" ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
